d43f0869b5f3a54b1619ac7be9c961edfa15f1ac
chen-zhi <chenzhi.finland@gmail.com>
2016-02-13 00:19:09 +0200

    修复点一次计算两次的bug.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-var score = 0;
-
-function right() {
-  score += 10;
-}
-
-document.getElementById("button").onclick = function() {result()};
-function result() {
-    alert("You scored " + score + "% correct");
+ function calculateScore()
+{
+  var score = 0;
+  if (document.getElementById('1').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('2').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('3').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('4').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('5').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('6').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('7').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('8').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('9').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('10').checked === true) {score += 10;} else { score += 0;}
+  
+  alert("You scored " + score + "% correct");
 }
