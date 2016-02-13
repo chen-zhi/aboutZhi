307a8f05af898bb1e0efacd18d4d2991a8c8bdbe
chen-zhi <chenzhi.finland@gmail.com>
2016-02-13 13:48:35 +0200

    改字体大小

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
  function calculateScore()
 {
   var score = 0;
-  if (document.getElementById('1').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('2').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('3').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('4').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('5').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('6').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('7').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('8').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('9').checked === true) {score += 10;} else { score += 0;}
-  if (document.getElementById('10').checked === true) {score += 10;} else { score += 0;}
+  if (document.getElementById('1').checked) {score += 10;}
+  if (document.getElementById('2').checked) {score += 10;}
+  if (document.getElementById('3').checked) {score += 10;}
+  if (document.getElementById('4').checked) {score += 10;}
+  if (document.getElementById('5').checked) {score += 10;}
+  if (document.getElementById('6').checked) {score += 10;}
+  if (document.getElementById('7').checked) {score += 10;}
+  if (document.getElementById('8').checked) {score += 10;}
+  if (document.getElementById('9').checked) {score += 10;}
+  if (document.getElementById('10').checked) {score += 10;}
   
   alert("You scored " + score + "% correct");
 }
